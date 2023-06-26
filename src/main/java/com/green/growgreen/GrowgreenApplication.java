07544d1afea4e55c7bcda4b6442722095080d9bc
김재경 <worud4227@naver.com>
2023-06-26 15:41:08 +0900

    jk new

---
+++ src/main/java/com/green/growgreen/GrowgreenApplication.java
@@ -0,0 +1,13 @@
+package com.green.growgreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowgreenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowgreenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/growgreen/GrowgreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.growgreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowgreenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
