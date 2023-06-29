572239cd94496d276e8e5c37f6d83efdda528849
김재경 <worud4227@naver.com>
2023-06-29 15:48:35 +0900

    jk todo dateformat

--- src/main/java/com/green/growgreen/todo/model/TodoVo.java
+++ src/main/java/com/green/growgreen/todo/model/TodoVo.java
@@ -6,10 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class TodoVo {
-    private int itodo;
-    private int iplant;
     private String ctnt;
-    private String deadline;
-    private String finishYn;
-
+    private String month;
+    private String time;
+    private String nickNm;
+    private String nm;
 }
